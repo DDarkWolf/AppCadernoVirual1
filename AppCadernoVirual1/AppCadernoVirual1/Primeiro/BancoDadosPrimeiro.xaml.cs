c267b6db4505ecaac374b9fe7902c05153e6e26c
Usuario <Usuario@CPD01-M01>
2022-06-13 15:01:04 -0300

    Adicionar arquivos de projeto.

---
+++ AppCadernoVirual1/AppCadernoVirual1/Primeiro/BancoDadosPrimeiro.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AppCadernoVirual1.Primeiro
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class BancoDadosPrimeiro : ContentPage
+    {
+        public BancoDadosPrimeiro()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushModalAsync(new PrimeiroAno());
+        }
+    }
+}
